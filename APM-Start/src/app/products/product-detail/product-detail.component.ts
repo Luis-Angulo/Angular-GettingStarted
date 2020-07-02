8b4f53979ca3572b70b71a0d454139adea4ec5f5
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-07-02 12:19:10 -0700

    Passing args via router

--- APM-Start/src/app/products/product-detail/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail/product-detail.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../IProduct';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   // No selector is needed, since this component will not be nested
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // + casts id to number
+    let id = +this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    this.pageTitle += `: ${id}`;
+    // HC product
+    this.product = {
+      productId: id,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2019',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    };
   }
-
 }
