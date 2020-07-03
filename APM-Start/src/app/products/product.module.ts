2ad7b0c615c37a35d57f732a06bc0acd16a609c1
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-07-02 17:22:09 -0700

    Refactoring shared module

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,33 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './products-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductsDetailGuard } from './products-detail.guard';
-
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ProductsListComponent,
-    ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
-  ],
+  declarations: [ProductsListComponent, ProductDetailComponent],
   imports: [
-    CommonModule,
-    FormsModule,
     // forChild prevents re-registering of the routing service in the app module
     RouterModule.forChild([
       { path: 'products', component: ProductsListComponent },
-      { path: 'products/:id',
-       component: ProductDetailComponent,
-       canActivate: [ProductsDetailGuard]
-       },
-    ])
-  ]
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+        canActivate: [ProductsDetailGuard],
+      },
+    ]),
+    SharedModule,
+  ],
 })
-export class ProductModule { }
+export class ProductModule {}
