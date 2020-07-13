d8c4160154c80257de90a290e9b2ddca0612fbd2
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-07-12 19:17:28 -0700

    refactored product routing

---
+++ APM-Start/src/app/products/product-routing.module.ts
@@ -0,0 +1,24 @@
+// Base angular modules (required)
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+// Routed components
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductsListComponent } from './products-list.component';
+import { ProductsDetailGuard } from './products-detail.guard';
+
+// Module config decorator
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      { path: 'products', component: ProductsListComponent },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+        canActivate: [ProductsDetailGuard],
+      },
+    ]),
+  ],
+  exports: [RouterModule],
+})
+export class ProductRoutingModule {}
--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ProductsListComponent } from './products-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { RouterModule } from '@angular/router';
-import { ProductsDetailGuard } from './products-detail.guard';
 import { SharedModule } from '../shared/shared.module';
+import { ProductRoutingModule } from './product-routing.module';
 
 @NgModule({
   declarations: [ProductsListComponent, ProductDetailComponent],
   imports: [
-    // forChild prevents re-registering of the routing service in the app module
-    RouterModule.forChild([
-      { path: 'products', component: ProductsListComponent },
-      {
-        path: 'products/:id',
-        component: ProductDetailComponent,
-        canActivate: [ProductsDetailGuard],
-      },
-    ]),
+    ProductRoutingModule,
     SharedModule,
   ],
 })
